6fa0c9e71d0c661fee53d188298b6661e120d184
Lars Tiefland <ltiefland@gmail.com>
2023-12-03 17:01:08 +0100

    Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,22 +1,30 @@
 <?php
 
-namespace Database\Seeders;
+    namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use Illuminate\Database\Seeder;
+    // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+    use App\Models\Comment;
+    use App\Models\Post;
+    use App\Models\User;
+    use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
+    class DatabaseSeeder extends Seeder
     {
-        // \App\Models\User::factory(10)->create();
+        /**
+         * Seed the application's database.
+         */
+        public function run(): void
+        {
+            $users = User::factory( 10 )->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+            $posts = Post::factory( 200 )->recycle( $users )->create();
+            $comments = Comment::factory( 100 )->recycle( $users )->recycle( $posts )->create();
+
+            \App\Models\User::factory()->create( [
+                'name'     => 'Lars Tiefland',
+                'email'    => 'ltiefland@gmail.com',
+                'password' => 'UNT_muw.kxt9cjf8tge',
+                //'password' => '$2y$12$8xRau4u0HaC5Miw8c2HO7Og9742wC0EM.4EAKiX6zR2jnwp2H/nMa',
+            ] );
+        }
     }
-}
