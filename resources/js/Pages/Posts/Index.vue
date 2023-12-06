1ed55db396e87d13bfeb66d8dff1a246177593ce
Lars Tiefland <ltiefland@gmail.com>
2023-12-06 21:38:47 +0100

    Laracsts

--- resources/js/Pages/Posts/Index.vue
+++ resources/js/Pages/Posts/Index.vue
@@ -5,7 +5,7 @@
             <h1>Posts</h1>
             <ul>
                 <li v-for="post in posts" :key="post.id">
-                    <inertia-link :href="rout('posts.show',post.id)">
+                    <inertia-link :href="route('posts.show',post.id)">
                         {{ post.title }}
                     </inertia-link>
                 </li>
