3621c06bab76ebdb79c953811f6b50597be9000e
Lars Tiefland <ltiefland@gmail.com>
2023-12-06 21:36:07 +0100

    Laracsts

--- resources/js/Pages/Posts/Index.vue
+++ resources/js/Pages/Posts/Index.vue
@@ -1,18 +1,17 @@
 <template>
 
     <AppLayout>
-        Hello world!
+        <div>
+            <h1>Posts</h1>
+            <ul>
+                <li v-for="post in posts" :key="post.id">
+                    <inertia-link :href="rout('posts.show',post.id)">
+                        {{ post.title }}
+                    </inertia-link>
+                </li>
+            </ul>
+        </div>
     </AppLayout>
-    <div>
-        <h1>Posts</h1>
-        <ul>
-            <li v-for="post in posts" :key="post.id">
-                <inertia-link :href="rout('posts.show',post.id)">
-                    {{ post.title }}
-                </inertia-link>
-            </li>
-        </ul>
-    </div>
 </template>
 <script setup>
 import AppLayout from "@/Layouts/AppLayout.vue";
