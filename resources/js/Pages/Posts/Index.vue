eaf42fb5a408b9d398afddce23e4740da9b2d1ad
Lars Tiefland <ltiefland@gmail.com>
2023-12-06 13:16:28 +0100

    Laraca

---
+++ resources/js/Pages/Posts/Index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <h1>Posts</h1>
+        <ul>
+            <li v-for="post in posts" :key="post.id">
+                <inertia-link :href="rout('posts.show',post.id)">
+                    {{ post.title }}
+                </inertia-link>
+            </li>
+        </ul>
+    </div>
+</template>
