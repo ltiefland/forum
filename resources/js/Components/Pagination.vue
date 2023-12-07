b30c5ec582c5b688f6a81d0f7ca2dae52c13df92
Lars Tiefland <ltiefland@gmail.com>
2023-12-07 20:59:40 +0100

    Laracsts

--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -27,7 +27,7 @@
             <div>
                 <nav class="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
                     <a  v-for="link in meta.links" :href="link.url"
-                       class="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0" v-html="link.label">
+                       class="relative inline-flex items-center first-of-type:rounded-l-md last-of-type:rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0" v-html="link.label">
                     </a>
                     <!-- Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" -->
                     <a href="#" aria-current="page"
