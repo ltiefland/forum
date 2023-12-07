4526968d93a5974de0f003f841e93b1406f40c27
Lars Tiefland <ltiefland@gmail.com>
2023-12-07 21:21:05 +0100

    Laracsts

--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -26,14 +26,16 @@
             </div>
             <div>
                 <nav class="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
-                    <a  v-for="link in meta.links" :href="link.url"
-                       class="relative inline-flex items-center first-of-type:rounded-l-md last-of-type:rounded-r-md px-3 py-2" v-html="link.label"
-                        :class="{
+                    <link v-for="link in meta.links" :href="link.url"
+                          class="relative inline-flex items-center first-of-type:rounded-l-md last-of-type:rounded-r-md px-3 py-2"
+                          v-html="link.label"
+                          preserve-scroll
+                          :class="{
                            'z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600' :link.active,
                            'text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0' : !link.active
                         }"
                     >
-                    </a>
+                    </link>
                 </nav>
             </div>
         </div>
@@ -42,6 +44,7 @@
 
 <script setup>
 import {ChevronLeftIcon, ChevronRightIcon} from '@heroicons/vue/20/solid'
+import {Link} from "@inertiajs/vue3";
 
 defineProps(["meta"]);
 
