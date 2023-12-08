7a769d9886a1eba6cdddc0e6688da71ae77ffc7e
Lars Tiefland <ltiefland@gmail.com>
2023-12-08 13:04:19 +0100

    Laracasts

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,26 +1,28 @@
 <?php
 
-namespace App\Providers;
+    namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
-use Illuminate\Http\Resources\Json\JsonResource;
+    use Illuminate\Support\ServiceProvider;
+    use Illuminate\Http\Resources\Json\JsonResource;
 
-class AppServiceProvider extends ServiceProvider
-{
-    /**
-     * Register any application services.
-     */
-    public function register(): void
+    class AppServiceProvider extends ServiceProvider
     {
-        //
-    }
+        /**
+         * Register any application services.
+         */
+        public function register(): void
+        {
+            //
+        }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot(): void
-    {
-        //
-        JsonResource::withoutWrapping();
+        /**
+         * Bootstrap any application services.
+         */
+        public function boot(): void
+        {
+            //
+            JsonResource::withoutWrapping();
+            Model::preventLazyLoading();
+            $this->withoutVite();
+        }
     }
-}
