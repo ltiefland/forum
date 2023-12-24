211058e6540f3ea723914896e2acb01ef95f7df4
Lars Tiefland <ltiefland@gmail.com>
2023-12-24 20:20:06 +0100

    Pagination component updated

--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -1,10 +1,12 @@
 <template>
-    <div class="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+Pa    <div class="flex items-center justify-between border-t border-gray-200 px-4 py-3 sm:px-6">
         <div class="flex flex-1 justify-between sm:hidden">
-            <Link :href="previousUrl" :only="only"
-               class="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">Previous</Link>
-            <Link :href="nextUrl" :only="only"
-               class="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">Next</Link>
+            <Link :href="previousUrl"
+                  :only="only"
+                  class="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">Previous</Link>
+            <Link :href="nextUrl"
+                  :only="only"
+                  class="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50">Next</Link>
         </div>
         <div class="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
             <div>
@@ -25,17 +27,16 @@
                 </p>
             </div>
             <div>
-                <nav class="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
+                <nav class="isolate inline-flex -space-x-px rounded-md shadow-sm bg-white" aria-label="Pagination">
                     <Link v-for="link in meta.links"
                           :only="only"
                           :href="link.url"
                           class="relative inline-flex items-center first-of-type:rounded-l-md last-of-type:rounded-r-md px-3 py-2"
                           :class="{
-                           'z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600' :link.active,
-                           'text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0' :!link.active,
-                           }"
+                           'z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600': link.active,
+                           'text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0': !link.active
+                       }"
                           v-html="link.label"
-                          preserve-scroll
                     >
                     </Link>
                 </nav>
@@ -49,9 +50,17 @@ import {ChevronLeftIcon, ChevronRightIcon} from '@heroicons/vue/20/solid'
 import {Link} from "@inertiajs/vue3";
 import {computed} from "vue";
 
-const props = defineProps(["meta","only"]);
+const props = defineProps({
+    meta: {
+        type: Object,
+        required: true
+    },
+    only: {
+        type: Array,
+        default: () => []
+    }
+});
 
-const previousUrl = computed(()=>props.meta.links[0].url);
-const nextUrl = computed(()=>props.meta.links.reverse()[0].url);
-
+const previousUrl = computed(() => props.meta.links[0].url);
+const nextUrl = computed(() => [...props.meta.links].reverse()[0].url);
 </script>
