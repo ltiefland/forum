1472c932f12182fd9e2aba0336ce50aeffe8448f
Lars Tiefland <ltiefland@gmail.com>
2024-02-19 13:38:52 +0100

    Laracasts Lesson 30 - part 1

--- resources/js/Components/MarkdownEditor.vue
+++ resources/js/Components/MarkdownEditor.vue
@@ -1,5 +1,74 @@
 <template>
-    <div class="bg-white rounded-md border-0 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600">
+    <div v-if="editor"
+         class="bg-white rounded-md border-0 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600">
+        <menu class="flex divide-x border-b">
+            <li>
+                <button @click="()=>editor.chain().focus().toggleBold().run()"
+                        type="button"
+                        :class="[
+                            editor.isActive('bold')?'bg-indigo-500 text-white' : 'hover:bg-gray-200'
+                        ]"
+                        class="px-3 py-2 rounded-tl-md"
+                        title="Bold">
+                    <i class="ri-bold"></i>
+                </button>
+            </li>
+            <li>
+                <button @click="()=>editor.chain().focus().toggleItalic().run()"
+                        type="button"
+                        :class="[
+                            editor.isActive('italic')?'bg-indigo-500 text-white' : 'hover:bg-gray-200'
+                        ]"
+                        class="px-3 py-2"
+                        title="Italic">
+                    <i class="ri-italic"></i>
+                </button>
+            </li>
+            <li>
+                <button @click="()=>editor.chain().focus().toggleStrike().run()"
+                        type="button"
+                        :class="[
+                            editor.isActive('strike')?'bg-indigo-500 text-white' : 'hover:bg-gray-200'
+                        ]"
+                        class="px-3 py-2"
+                        title="Strikethrough">
+                    <i class="ri-strikethrough"></i>
+                </button>
+            </li>
+            <li>
+                <button @click="()=>editor.chain().focus().toggleBlockquote().run()"
+                        type="button"
+                        :class="[
+                            editor.isActive('blockquote')?'bg-indigo-500 text-white' : 'hover:bg-gray-200'
+                        ]"
+                        class="px-3 py-2"
+                        title="Blockquote">
+                    <i class="ri-double-quotes-l"></i>
+                </button>
+            </li>
+            <li>
+                <button @click="()=>editor.chain().focus().toggleBulletList().run()"
+                        type="button"
+                        :class="[
+                            editor.isActive('bulletList')?'bg-indigo-500 text-white' : 'hover:bg-gray-200'
+                        ]"
+                        class="px-3 py-2"
+                        title="Bullet list">
+                    <i class="ri-list-unordered"></i>
+                </button>
+            </li>
+            <li>
+                <button @click="()=>editor.chain().focus().toggleOrderedList().run()"
+                        type="button"
+                        :class="[
+                            editor.isActive('orderedList')?'bg-indigo-500 text-white' : 'hover:bg-gray-200'
+                        ]"
+                        class="px-3 py-2"
+                        title="Numeric list">
+                    <i class="ri-list-ordered"></i>
+                </button>
+            </li>
+        </menu>
         <EditorContent :editor="editor"/>
     </div>
 </template>
@@ -8,6 +77,7 @@ import {useEditor, EditorContent} from '@tiptap/vue-3'
 import StarterKit from '@tiptap/starter-kit'
 import {watch} from "vue";
 import {Markdown} from "tiptap-markdown";
+import 'remixicon/fonts/remixicon.css';
 
 const props = defineProps({
     modelValue: '',
@@ -31,8 +101,7 @@ const editor = useEditor({
 });
 
 watch(() => props.modelValue, (value) => {
-    if(value===editor.value?.storage.markdown.getMarkdown())
-    {
+    if (value === editor.value?.storage.markdown.getMarkdown()) {
         return;
     }
     editor.value?.commands.setContent(value);
