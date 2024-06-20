f756bef0e6003fbb0def0239ac48771830344a15
Lars Tiefland <ltiefland@gmail.com>
2024-06-20 13:16:33 +0200

    User ltiefland@gmail.com restored

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,38 +1,48 @@
 <?php
 
-namespace Database\Seeders;
+    namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\Comment;
-use App\Models\Post;
-use App\Models\Topic;
-use App\Models\User;
-use Illuminate\Database\Seeder;
+    // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+    use App\Models\Comment;
+    use App\Models\Post;
+    use App\Models\Topic;
+    use App\Models\User;
+    use Illuminate\Database\Seeder;
 
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
+    class DatabaseSeeder extends Seeder
     {
-        $this->call(TopicSeeder::class);
-        $topics = Topic::all();
+        /**
+         * Seed the application's database.
+         */
+        public function run(): void
+        {
+            $this->call( TopicSeeder::class );
+            $topics = Topic::all();
 
-        $users = User::factory(10)->create();
+            $users = User::factory( 10 )->create();
 
-        $posts = Post::factory(200)
-            ->withFixture()
-            ->has(Comment::factory(15)->recycle($users))
-            ->recycle([$users, $topics])
-            ->create();
+            $posts = Post::factory( 200 )
+                         ->withFixture()
+                         ->has( Comment::factory( 15 )->recycle( $users ) )
+                         ->recycle( [ $users, $topics ] )
+                         ->create();
 
-        $luke = User::factory()
-            ->has(Post::factory(45)->recycle($topics)->withFixture())
-            ->has(Comment::factory(120)->recycle($posts))
-            ->create([
-                'name' => 'Luke Downing',
-                'email' => 'test@example.com',
-            ]);
+            User::factory()->create( [
+                                         'name'                      => 'Lars Tiefland',
+                                         'email'                     => 'ltiefland@gmail.com',
+                                         'password'                  => '$2y$12$8xRau4u0HaC5Miw8c2HO7Og9742wC0EM.4EAKiX6zR2jnwp2H/nMa',
+                                         'two_factor_secret'         => 'eyJpdiI6Ik1FellDaEdZZkx5dmE0K3Y0OHkvSkE9PSIsInZhbHVlIjoiVHJ1VG15WEVqZmN2ZXVpY2ZjU3FJcnVyYldlY0xOTExKeFUwR2RHeURWWT0iLCJtYWMiOiIxMWY0YTE0YmQ5YjJiN2Y2Y2I4MDVlZDU1NWI4NGIyZjZmNTdhN2VkOTJkOWY5ZjYzOWQ0MDU4NTcxZWJlN2U5IiwidGFnIjoiIn0=',
+                                         'two_factor_recovery_codes' => 'eyJpdiI6IlQvT2l6aWlYM081dUxmZDB1ajhjUHc9PSIsInZhbHVlIjoiL0ZsSTFwUm51dDdiTGVaZXJNdVo3YnFObXR4NWVXcHU1VWRCQ3JlZ1ZnTjlObWkvaEtRZUFjdlp1aEhSZGxPNXRBUGVieE04dHJVcnU4WS92QnVCMmdzODgyd1FHZ2RKMnM0MU50dEpCTElMZzRaT0RYS3VDZDhKcUVWaXAzMHRtOUNYUUZoSHVibHdXbUtKN3NpZ1N1MEFMR1BscXZLdHNnQng3aXBlNWFZV3BuVFBTRkppaUVNM2RKSTFUZEZMbmdiOWZPejdZWTY5MlJoekMwK0lvSUJReHUySGxCUmg4NEJiTUQ1cC9LZWZpbFI4WFZ5RWpadE1UMXI5YkRXc1l4Qno2RE8yd1AwYTNuWTlXcTJvUHc9PSIsIm1hYyI6IjM4NGVlZjQyZTRjMDQ3MzI3ZjdjYjlmMDBmNDc1MjExNTJlODc2MDU3OGZkNjQ5NDk0NGMwOWU5YzJkZTcxZDYiLCJ0YWciOiIifQ==',
+                                         'two_factor_confirmed_at'   => '2023-12-02 23:55:49',
+                                         'remember_token'            => null,
+                                         'profile_photo_path'        => 'profile-photos/lbQM2z7AbVlG6APT6ZFdh4PUUbjKEzE9EzVFEJrz.jpg',
+                                     ] );
+            $luke = User::factory()
+                        ->has( Post::factory( 45 )->recycle( $topics )->withFixture() )
+                        ->has( Comment::factory( 120 )->recycle( $posts ) )
+                        ->create( [
+                                      'name'  => 'Luke Downing',
+                                      'email' => 'test@example.com',
+                                  ] );
+        }
     }
-}
