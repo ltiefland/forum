0dc95bfd671bc818b6213e0d8298e2aa9f913f7b
Lars Tiefland <ltiefland@gmail.com>
2024-06-25 12:41:40 +0200

    Laracasts Lesson 41

--- app/Support/PostFixtures.php
+++ app/Support/PostFixtures.php
@@ -1,23 +1,21 @@
 <?php
 
-namespace App\Support;
+    namespace App\Support;
 
-use Illuminate\Support\Collection;
-use Illuminate\Support\Facades\File;
-use Symfony\Component\Finder\SplFileInfo;
+    use Illuminate\Support\Collection;
+    use Illuminate\Support\Facades\File;
+    use Symfony\Component\Finder\SplFileInfo;
 
-class PostFixtures
-{
-    private static Collection $fixtures;
-
-    public static function getFixtures(): Collection
+    class PostFixtures
     {
-        return self::$fixtures ??= collect(File::files(database_path('factories/fixtures/posts')))
-            ->map(fn (SplFileInfo $fileInfo) => $fileInfo->getContents())
-            ->map(fn (string $contents) => str($contents)->explode("\n", 2))
-            ->map(fn (Collection $parts) => [
-                'title' => str($parts[0])->trim()->after('# '),
-                'body' => str($parts[1])->trim(),
-            ]);
+        public static function getFixtures(): Collection
+        {
+            return once( fn() => collect( File::files( database_path( 'factories/fixtures/posts' ) ) )
+                ->map( fn( SplFileInfo $fileInfo ) => $fileInfo->getContents() )
+                ->map( fn( string $contents ) => str( $contents )->explode( "\n", 2 ) )
+                ->map( fn( Collection $parts ) => [
+                    'title' => str( $parts[0] )->trim()->after( '# ' ),
+                    'body'  => str( $parts[1] )->trim(),
+                ] ) );
+        }
     }
-}
