efceffc28c4024dcfb02bb482bc9add7eb529857
Lars Tiefland <ltiefland@gmail.com>
2024-07-02 13:47:14 +0200

    Laracasts Lesson 43

--- app/Http/Resources/PostResource.php
+++ app/Http/Resources/PostResource.php
@@ -1,31 +1,31 @@
 <?php
 
-namespace App\Http\Resources;
+    namespace App\Http\Resources;
 
-use Illuminate\Http\Request;
-use Illuminate\Http\Resources\Json\JsonResource;
+    use Illuminate\Http\Request;
+    use Illuminate\Http\Resources\Json\JsonResource;
 
-class PostResource extends JsonResource
-{
-    /**
-     * Transform the resource into an array.
-     *
-     * @return array<string, mixed>
-     */
-    public function toArray(Request $request): array
+    class PostResource extends JsonResource
     {
-        return [
-            'id' => $this->id,
-            'user' => $this->whenLoaded('user', fn () => UserResource::make($this->user)),
-            'topic' => $this->whenLoaded('topic', fn () => TopicResource::make($this->topic)),
-            'title' => $this->title,
-            'body' => $this->body,
-            'html' => $this->html,
-            'updated_at' => $this->updated_at,
-            'created_at' => $this->created_at,
-            'routes' => [
-                'show' => $this->showRoute(),
-            ],
-        ];
+        /**
+         * Transform the resource into an array.
+         *
+         * @return array<string, mixed>
+         */
+        public function toArray( Request $request ): array
+        {
+            return [
+                'id'         => $this->id,
+                'user'       => UserResource::make( $this->whenLoaded( 'user' ) ),
+                'topic'      => TopicResource::make( $this->whenLoaded( 'topic' ) ),
+                'title'      => $this->title,
+                'body'       => $this->body,
+                'html'       => $this->html,
+                'updated_at' => $this->updated_at,
+                'created_at' => $this->created_at,
+                'routes'     => [
+                    'show' => $this->showRoute(),
+                ],
+            ];
+        }
     }
-}
