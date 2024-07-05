68db9a2f650653bb6d5beb260d8a47b49c07202d
Lars Tiefland <ltiefland@gmail.com>
2024-07-05 13:32:22 +0200

    Laracasts Lesson 46

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
     use App\Http\Controllers\CommentController;
+    use App\Http\Controllers\LikeController;
     use App\Http\Controllers\PostController;
     use Illuminate\Foundation\Application;
     use Illuminate\Support\Facades\Route;
@@ -40,6 +41,7 @@
 
         Route::resource( 'posts', PostController::class )->only( [ 'create', 'store' ] );
         Route::resource( 'posts.comments', CommentController::class )->shallow()->only( [ 'store', 'update', 'destroy' ] );
+        Route::post('/likes/{type}/{id}',[LikeController::class,'store'])->name('likes.store');
     } );
 
     Route::get( 'posts/{post}/{slug}', [ PostController::class, 'show' ] )->name( 'posts.show' );
